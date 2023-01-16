a818a769a67ac80f3b84f91bb3f14b86105fd529
Harleen Kaur <harleen.kaur9795@gmail.com>
2023-01-16 16:55:11 +0530

    Updates

---
+++ precache-manifest.394c507ad7900055d7fb703b308891d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "105e1460a4a3186ab750",
+    "url": "/TextUtils-React/static/css/main.2acbe3d1.chunk.css"
+  },
+  {
+    "revision": "105e1460a4a3186ab750",
+    "url": "/TextUtils-React/static/js/main.105e1460.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/TextUtils-React/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "4b3c2d27ce7566aa7f06",
+    "url": "/TextUtils-React/static/js/2.4b3c2d27.chunk.js"
+  },
+  {
+    "revision": "c664369cb110a7f6239b",
+    "url": "/TextUtils-React/static/js/runtime~main.c664369c.js"
+  },
+  {
+    "revision": "623b80cdf4b6091e1320e153bc981502",
+    "url": "/TextUtils-React/index.html"
+  }
+];
