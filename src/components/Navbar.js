d3d3fa15d369bbf5e4892fca6d5ed7ff40b765f4
Harleen Kaur <harleen.kaur9795@gmail.com>
2023-01-17 12:04:37 +0530

    Removed routing href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="">
           {props.title}
         </a>
         <button
@@ -30,7 +30,7 @@ export default function Navbar(props) {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/about">
+              <a className="nav-link" href="">
                 {props.aboutText}
               </a>
             </li>
