900a978b465b449a83d93ae8e736dcd3533caa75
Harleen Kaur <harleen.kaur9795@gmail.com>
2023-01-17 12:13:26 +0530

    Removed the home routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,10 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="">
+        {/* <Link className="navbar-brand" to="/">
+          {props.title}
+        </Link> */}
+        <a className="navbar-brand" href="/">
           {props.title}
         </a>
         <button
@@ -25,15 +28,18 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
+              {/* <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link> */}
               <a className="nav-link active" aria-current="page" href="/">
                 Home
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="">
+            {/* <li className="nav-item">
+              <Link className="nav-link" to="/about">
                 {props.aboutText}
-              </a>
-            </li>
+              </Link>
+            </li> */}
           </ul>
           {/* <form className="d-flex" role="search">
             <input
