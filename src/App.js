aae7f164afd685d0601013852ea12759b9c365ea
Harleen Kaur <harleen.kaur9795@gmail.com>
2023-01-17 12:01:30 +0530

    My First Commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Alert from "./components/Alert";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not.
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled !", "success");
+      document.title = "TextUtils - Dark Mode";
+      // setInterval(() => {
+      //   document.title = "TextUtils is Amazing Mode";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils now";
+      // }, 1500);
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled !", "success");
+      document.title = "TextUtils - Light Mode";
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router> */}
+        {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+        {/* <Navbar /> */}
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />} />  */}
+            {/* 
+              Here, "exact" keyword is used because react js does partial matching...
+              For Example: if one path is /users and the other path is /users/home
+              then even if we click on /users/home it will redirect to /users..
+            */}
+            {/* <Route exact path="/" element={  */}
+            <TextForm showAlert={showAlert} heading="Enter the text to analyse below" mode={mode} />
+            {/* } /> */}
+          {/* </Routes> */}
+        </div>
+      {/* </Router> */}
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  let [count, setCount] = useState(0);
+  let [count1, setCount1] = useState(0);
+  let countChar = 0,
+    countCons = 0;
+
+  const [text, setText] = useState("");
+  // text = "New Text"; // Wrong way to change the state
+  // setText("New Text"); // Correct way to change the state
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked !" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to Uppercase !","success");
+  };
+  const handleLoClick = () => {
+    let newText = text.toLocaleLowerCase();
+    setText(newText);
+    props.showAlert("Converted to Lowercase !","success");
+  };
+  const handleVoClick = () => {
+    for (count = 0; count <= text.length; count++) {
+      if (text.charAt(count).match(/[aeiouAEIOU]/)) {
+        countChar++;
+        setCount(countChar);
+        props.showAlert("Counted the no. of Vowels !","success");
+      }
+    }
+    // console.log("No. of Vowels are: " + countChar);
+  };
+  const handleCoClick = () => {
+    for (count1 = 0; count1 <= text.length; count1++) {
+      if (
+        text
+          .charAt(count1)
+          .match(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)
+      ) {
+        countCons++;
+        setCount1(countCons);
+      }
+    }    
+    props.showAlert("Counted the no. of Consonants !","success");
+    // console.log("No. of consonants are: " + countCons);
+  };
+  const handleOnChange = (event) => {
+    // console.log("On Change !");
+    setText(event.target.value);
+  };
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
+        <div className="mb-3">
+          <h2>{props.heading}</h2>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "#042743" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleVoClick}>
+          Count no. of Vowels
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCoClick}>
+          Count no. of Consonants
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
+        <h3>Your text summary</h3>
+        <p>
+          {text.split(" ").length} words, {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h3>You have entered:</h3>
+        <p>{count} No. of Vowels</p>
+        <p>{count1} No. of Consonants</p>
+        <h3>Preview</h3>
+        <p>{text.length>0?text:'Enter something in the textbox to preview ir her'}</p>
+      </div>
+    </>
+  );
+}
