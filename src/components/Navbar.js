1740697555da28f8ac72ab01f4ad306e0010984f
Harleen Kaur <harleen.kaur9795@gmail.com>
2023-01-17 15:34:50 +0530

    changing value of href from / to #

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar(props) {
         {/* <Link className="navbar-brand" to="/">
           {props.title}
         </Link> */}
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {props.title}
         </a>
         <button
@@ -31,7 +31,7 @@ export default function Navbar(props) {
               {/* <Link className="nav-link active" aria-current="page" to="/">
                 Home
               </Link> */}
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
